a7f6b46eb89113445533d8c7155ed7db551b9c87
Usuário Ciencias da Computação <computacao@unirp.com.br>
2026-09-15 21:18:48 -0300

    Cria estrutura inicial do projeto

---
+++ README.md
@@ -0,0 +1,3 @@
+# Classificador de mensagens
+ 
+Projeto desenvolvido durante a aula pratica de Git e GitHub.
---
+++ modelo.py
@@ -0,0 +1,5 @@
+nome_modelo = "Classificador de mensagens"
+accuracy = 0.80
+ 
+print(f"Modelo: {nome_modelo}")
+print(f"Acurácia: {accuracy}")
